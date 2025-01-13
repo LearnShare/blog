ca2865a8259f09e4487ae060386ee9b4e5fb8454
LearnShare <learnshare@126.com>
2025-01-13 15:12:34 +0800

    update: site sidebar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,53 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {
+  defineConfig,
+} from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightSidebarTopics from 'starlight-sidebar-topics';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'LearnShare',
-			social: {
-				github: 'https://github.com/LearnShare/blog',
-			},
-			pagination: false,
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'LearnShare',
+      social: {
+        github: 'https://github.com/LearnShare/blog',
+      },
+      pagination: false,
+      plugins: [
+        starlightSidebarTopics([
+          {
+            label: '文章',
+            link: '/posts',
+            icon: 'open-book',
+            items: [
+              {
+                label: '全部文章',
+                autogenerate: {
+                  directory: 'posts'
+                },
+              },
+            ],
+          },
+          {
+            label: '专题',
+            link: '/topics',
+            icon: 'list-format',
+            items: [
+              {
+                label: 'Design System',
+                collapsed: true,
+                items: [
+                  {
+                    label: '概述',
+                    link: '/topics/design-system',
+                  },
+                ],
+              },
+            ],
+          },
+        ]),
+      ],
+    }),
+  ],
 });
